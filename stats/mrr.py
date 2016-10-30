309bc5a12fd50ba5696edc7d0d72cfcc9330b74a
eliazarinelli <elia.zarinelli@gmail.com>
2016-10-30 16:57:00 +0000

    added mrr generator

--- stats/mrr.py
+++ stats/mrr.py
@@ -1,13 +1,13 @@
 import random
 
 
-def dar(n, rho=0.5, m=0., start=1):
+def dar(n, m=0., rho=0.5, start=1):
 	"""
 	Generator of a DAR(1) process
 
 	:param n: int, length of the series
-	:param rho: float, correlation parameter
 	:param m: float, average parameter
+	:param rho: float, correlation parameter
 	:param start: int, starting value of the series
 	:return: int, +1 or -1
 	"""
@@ -39,3 +39,25 @@ def dar(n, rho=0.5, m=0., start=1):
 
 		epsilon_previous = epsilon_new
 		i += 1
+
+
+def mrr(n, m=0., rho=0.5, theta=1., phi=1., sigma=1.):
+
+	c_l = theta + phi
+	c_r = -1.*(rho * theta + phi)
+
+	epsilon_gen = dar(n+1, m=m, rho=rho, start=1)
+
+	epsilon_previous = next(epsilon_gen)
+	i = 0
+
+	while i < n:
+		epsilon_new = next(epsilon_gen)
+		yield epsilon_new, c_l * float(epsilon_new) + c_r * float(epsilon_previous) + random.gauss(0., sigma)
+		epsilon_previous = epsilon_new
+
+if __name__ == '__main__':
+
+	a = mrr(100, rho=0., theta=0., phi=1., sigma=0.)
+	for i, j in a:
+		print(i,j)
