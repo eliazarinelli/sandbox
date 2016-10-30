5cea5599e44a62b550a95cd92bb54629fe65b145
eliazarinelli <elia.zarinelli@gmail.com>
2016-10-30 18:10:03 +0000

    adding moments functions

--- stats/mrr.py
+++ stats/mrr.py
@@ -1,5 +1,77 @@
 import random
 
+def _c_l(theta, phi):
+	return theta + phi
+
+
+def _c_r(rho, theta, phi):
+	return -1.*(rho * theta + phi)
+
+
+def _c_2(m):
+	return 1. - m**2
+
+
+def _c_11(m, rho):
+	return rho*(1.-m**2)
+
+
+def _c_101(m, rho):
+	return rho**2*(1.-m**2)
+
+
+def _c_1001(m, rho):
+	return rho**3*(1.-m**2)
+
+
+def _c_3(m):
+	return -2.*m*(1.-m**2)
+
+
+def _c_21(m, rho):
+	return -2.*m*(1-m**2)*rho
+
+
+def _c_4(m):
+	return (1.+3.*m**2)*(1.-m**2)
+
+
+def _c_31(m, rho):
+	return rho*(1.+3.*m**2)*(1.-m**2)
+
+
+def _c_22(m, rho):
+	return 1.-2.*m**2+m**4+4.*m**2*rho*(1.-m**2)
+
+
+def _gm_1_1(m, rho, theta, phi, sigma):
+	return sigma**2 + (_c_l(theta, phi)**2 + _c_r(rho, theta, phi)**2)*_c_2(m) \
+			+ 2.*_c_l(theta, phi)*_c_r(rho, theta, phi)*_c_11(m, rho)
+
+
+def _gm_1_2(m, rho, theta, phi):
+	return (_c_l(theta, phi)**3+_c_r(rho, theta, phi)**3)*_c_3(m) \
+		   + 3.*(_c_l(theta, phi)+_c_r(rho, theta, phi))*_c_21(m, rho)
+
+
+def _gm_1_3(m, rho, theta, phi, sigma):
+	return (_c_l(theta, phi)**4+_c_r(rho, theta, phi)**4)*_c_4(m) \
+			+ 4.*_c_l(theta, phi)*_c_r(rho, theta, phi)*(_c_l(theta, phi)**2+_c_r(rho, theta, phi)**2)*_c_31(m,rho) \
+			+ 6.*_c_l(theta, phi)**2*_c_r(rho, theta, phi)**2*_c_22(m,rho) \
+			+ 6.*sigma**2*((_c_l(theta, phi)**2+_c_r(rho, theta, phi)**2)*_c_2(m) + 2.*_c_l(theta, phi)*_c_r(rho, theta, phi)*_c_11(m,rho)) \
+			+ 3.*sigma**4
+
+
+def _gm_1_4(m, rho, theta, phi, sigma):
+	return (_c_l(theta, phi)**2+_c_r(rho, theta, phi)**2)*_c_11(m, rho) \
+			+ _c_l(theta, phi)*_c_r(rho, theta, phi)*(_c_11(m, rho)+_c_101(m, rho))
+
+
+def _gm_1_5(m, rho, theta, phi, sigma):
+	return (_c_l(theta, phi)**2+_c_r(rho, theta, phi)**2)*_c_101(m, rho) \
+			+ _c_l(theta, phi)*_c_r(rho, theta, phi)*(_c_11(m, rho)+_c_1001(m, rho))
+
+
 
 def dar(n, m=0., rho=0.5, start=1):
 	"""
@@ -43,6 +115,18 @@ def dar(n, m=0., rho=0.5, start=1):
 
 def mrr(n, m=0., rho=0.5, theta=1., phi=1., sigma=1.):
 
+	"""
+	Generator of the MRR process
+
+	:param n: number of steps
+	:param m: dar mean parameter
+	:param rho: dar correlation parameter
+	:param theta: impact parameter
+	:param phi: spread parameter
+	:param sigma: volatility parameter
+	:return: the trade initiation variable epsilon_i and the price return y_i
+	"""
+
 	c_l = theta + phi
 	c_r = -1.*(rho * theta + phi)
 
@@ -55,6 +139,9 @@ def mrr(n, m=0., rho=0.5, theta=1., phi=1., sigma=1.):
 		epsilon_new = next(epsilon_gen)
 		yield epsilon_new, c_l * float(epsilon_new) + c_r * float(epsilon_previous) + random.gauss(0., sigma)
 		epsilon_previous = epsilon_new
+
+
+
 
 if __name__ == '__main__':
 
---
+++ tests/test_mrr.py
@@ -0,0 +1,42 @@
+import unittest
+import stats.mrr as mrr
+
+
+class TestDar(unittest.TestCase):
+
+	def test_rho_1(self):
+
+		""" if rho=1 we expect all the spins to be equal to start """
+
+		n_steps = 100
+		dd = mrr.dar(n=n_steps, m=0., rho=1., start=1)
+		n_up_down = sum(dd)
+		self.assertEqual(n_up_down, n_steps)
+
+		dd = mrr.dar(n=n_steps, m=0., rho=1., start=-1)
+		n_up_down = sum(dd)
+		self.assertEqual(n_up_down, -1*n_steps)
+
+
+class TestMrr(unittest.TestCase):
+
+	def test_no_noise(self):
+
+		""" If sigma=0 we expect all the trade price returns to be in
+		[theta*(1-rho), theta*(1+rho)+2phi, -(1+rho)theta-2phi, -theta*(1-rho)] """
+
+		n_steps = 100
+		m_in = 0.
+		phi_in = 1.
+		rho_in = 0.5
+		theta_in = 1.
+		phi_in = 1.
+		sigma_in = 0.
+
+		mm = mrr.mrr(n=n_steps, m=m_in, rho=rho_in, theta=theta_in, phi=phi_in, sigma=sigma_in)
+
+		expected_values = [theta_in*(1.-rho_in), theta_in*(1.+rho_in)+2.*phi_in,
+							-(1.+rho_in)*theta_in-2.*phi_in, -1.*theta_in*(1.-rho_in)]
+
+		for i, j in mm:
+			self.assertIn(j, expected_values)
