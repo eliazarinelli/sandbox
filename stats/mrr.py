98f2bd583ec4fde5214df48c005a602c724288e2
eliazarinelli <elia.zarinelli@gmail.com>
2016-11-08 23:09:35 +0000

    removing unused functions

--- stats/mrr.py
+++ stats/mrr.py
@@ -3,10 +3,14 @@ import numpy as np
 import scipy.stats
 import scipy.optimize
 
-CL_START = 2.
-CR_START = -2.
-SIGMA_START = 2.
 DEFAULT_VAL = -10000.
+
+START_CL = 2.
+START_CR = -2.
+START_SIGMA = 2.
+START_PREFACTOR = -1.
+START_RHO = 0.5
+
 
 def _c_l(theta, phi):
 	return theta + phi
@@ -148,59 +152,6 @@ def mrr(n, m=0., rho=0.5, theta=1., phi=1., sigma=1.):
 		epsilon_previous = epsilon_new
 
 
-def estimate_moments(sample):
-
-	"""
-	Estimate the moments of the transaction price returns of a MRR process
-
-	:param sample: iterable of the trade initiation variable espilon_i
-		and transaction price return y_i
-	:return: tuple of the moments y^2, y^3, y^4, y*y(-1), y*y(-2)
-	"""
-	mm_1_1 = 0.
-	mm_1_2 = 0.
-	mm_1_3 = 0.
-	mm_1_4 = 0.
-	mm_1_5 = 0.
-
-	y_p = 0.
-	y_pp = 0.
-
-	count = 0
-
-	for e, y in sample:
-		count += 1
-		mm_1_1 += y**2
-		mm_1_2 += y**3
-		mm_1_3 += y**4
-		mm_1_4 += y*y_p
-		mm_1_5 += y*y_pp
-		y_pp = y_p
-		y_p = y
-
-	return mm_1_1/(1.*count), mm_1_2/(1.*count), mm_1_3/(1.*count), mm_1_4/(1.*count-1.), mm_1_5/(1.*count-2.)
-
-
-def evaluate_moments(m, rho, theta, phi, sigma):
-	"""
-	Evaluate the moments of the transaction price returns of a MRR process
-	:param m: dar mean parameter
-	:param rho: dar correlation parameter
-	:param theta: impact parameter
-	:param phi: spread parameter
-	:param sigma: volatility parameter
-	:return: tuple of the moments y^2, y^3, y^4, y*y(-1), y*y(-2)
-	"""
-
-	output = (
-		_gm_1_1(m, rho, theta, phi, sigma), _gm_1_2(m, rho, theta, phi),
-		_gm_1_3(m, rho, theta, phi, sigma), _gm_1_4(m, rho, theta, phi),
-		_gm_1_5(m, rho, theta, phi)
-	)
-
-	return output
-
-
 ######################################################################################################
 
 
@@ -250,14 +201,14 @@ def _fit_acv_population(xx, acv_sample):
 	"""
 
 	if len(xx) != len(acv_sample):
-		raise ValueError('xx and yy have different lenght')
+		raise ValueError('xx and yy have different length')
 
 	# casting to np.array
 	xx_in = np.array(xx)
 	acv_sample_in = np.array(acv_sample)
 
 	# non-linear fit
-	out = scipy.optimize.curve_fit(_acv_population, xx_in, acv_sample_in, [-1., 1.])
+	out = scipy.optimize.curve_fit(_acv_population, xx_in, acv_sample_in, [START_PREFACTOR, START_RHO])
 
 	# return the prefactor and rho
 	return out[0][0], out[0][1]
@@ -287,87 +238,8 @@ def estimate_rho_qq(sample, n_lags):
 	return rho_hat, prefactor_hat*rho_hat
 
 
-def _estimate_vkq(sample):
-
-	"""
-	Estimate the sample second and fourth central moments and 1-lag covariance
-
-	:param sample: list, a sample of transaction price returns
-	:return: tuple, sample moments
-	"""
-
-	# second central moment
-	vv_e = np.var(sample)
-
-	# fourth central moment
-	kk_e = scipy.stats.kurtosis(sample, fisher=False) * vv_e**2
-
-	# 1-lag covariance
-	q1_e = _estimate_acv(sample, 1)[1]
-
-	return vv_e, kk_e, q1_e
-
-
-def _fit_mean(x, y):
-
-	"""
-	Average the slope of the linear fit of y vs x
-
-	:param x: list, x values
-	:param y: list, y values
-	:return: float, average slope
-	"""
-
-	tmp = []
-	for i in range(3, len(x)+1):
-		tmp.append(scipy.stats.linregress(x[:i], y[:i]))
-	tmp_2 = list(zip(*tmp))
-	#return np.mean(np.exp(tmp))
-	return np.mean(tmp_2[0]), np.mean(tmp_2[1])
-
-
-def _estimate_rho(sample, n_lags):
-
-	"""
-	Estimate the sample rho
-
-	:param sample: list, transaction price returns
-	:param n_lags: int, number of lags for the fitting
-	:return: float, rho hat
-
-	Find the best fitting parameter rho_i that describe the decay
-	of the first i lags of the sample auto-covariance function (times -1).
-	Repeat the procedure for 3<i<n_lags.
-	Returns the average of the estimated rho_i.
-	"""
-
-	# auto-covariance of the sample
-	acv = _estimate_acv(sample, n_lags)[1:]
-
-	# positive auto-covariance
-	yy_0 = np.array([-1.*i for i in acv])
-
-	# list of integers from 1 to n
-	xx_0 = np.arange(1, len(acv)+1)
-
-	# removing negative p_acv
-	sel_pos = yy_0 > 0.
-	xx_1 = xx_0[sel_pos]
-	yy_1 = yy_0[sel_pos]
-
-	if len(xx_1) < 3:
-		# if the number of non-negative values is less than 3, we return nan
-		# since it would be impossible to fit rho
-		print('Not enough positive points, return nan')
-		return np.nan
-	else:
-		# rho hat
-		rho_sample_log = _fit_mean(xx_1, np.log(yy_1))
-		#return rho_sample_log
-		return np.exp(rho_sample_log[0]), np.exp(rho_sample_log[1])
-
-
 ######################################################################################################
+
 
 def _vv(x, y, s, r):
 	return s**2 + (x+y)**2 + 2.*(r-1.)*x*y
@@ -398,8 +270,11 @@ def _find_params(params_start, *sample_moments_rho):
 	:param sample_moments_rho: list, estimated sample moments vv, kk and qq and the estimated rho
 	:return: tuple, inferred cl, cr and sigma
 	"""
-
-	return tuple(scipy.optimize.fsolve(_equations, x0=params_start, args=sample_moments_rho))
+	try:
+		out = tuple(scipy.optimize.fsolve(_equations, x0=params_start, args=sample_moments_rho))
+	except RuntimeWarning:
+		out = (-1000., -1000., -1000.)
+	return out
 
 
 def _solve_parameters(vv_sample, kk_sample, qq_sample, rho_hat):
@@ -417,14 +292,14 @@ def _solve_parameters(vv_sample, kk_sample, qq_sample, rho_hat):
 	moments_rho_sample = (vv_sample, kk_sample, qq_sample, rho_hat)
 
 	# starting value of the inference parameter
-	params_start = [CL_START, CR_START, SIGMA_START]
+	params_start = [START_CL, START_CR, START_SIGMA]
 
 	# default value of the sample parameters
 	theta_hat = DEFAULT_VAL
 	phi_hat = DEFAULT_VAL
 	sigma_hat = DEFAULT_VAL
 
-	while theta_hat < 0. or phi_hat < 0. or sigma_hat < 0.:
+	while theta_hat < 0.1 or phi_hat < 0. or sigma_hat < 0.:
 
 		# find the parameters cl, cr and sigma that better explain the sample moments
 		cl_hat, cr_hat, sigma_hat = _find_params(params_start, moments_rho_sample)
@@ -437,9 +312,6 @@ def _solve_parameters(vv_sample, kk_sample, qq_sample, rho_hat):
 
 		# new starting parameters
 		params_start = [i + np.random.normal(0., 0.1) for i in params_start]
-		#params_start[0] = np.abs(params_start[0] + np.random.normal(0., 1.))
-		#params_start[1] = -1.*np.abs(params_start[1] + np.random.normal(0., 1.))
-		#params_start[2] = np.abs(params_start[2] + np.random.normal(0., 1.))
 
 	return theta_hat, phi_hat, sigma_hat
 
@@ -458,61 +330,3 @@ def estimate_parameters(sample, n_lag_rho):
 		# parameters estimation
 		theta_hat, phi_hat, sigma_hat = _solve_parameters(vv_sample, kk_sample, qq_sample, rho_hat)
 		return rho_hat, theta_hat, phi_hat, sigma_hat
-
-
-if __name__ == '__main__':
-
-	n_steps = 2**18
-
-	m = 0.
-	rho = 0.7
-	theta = 1.
-	phi = 0.3
-	sigma = 1.
-
-	if True:
-
-		sample_gen = mrr(n=n_steps, m=m, rho=rho, theta=theta, phi=phi, sigma=sigma)
-		sample_list = list(zip(*sample_gen))
-		sample = sample_list[1]
-
-		qq_hat, rho_hat = estimate_rho_qq(sample, 10)
-
-		qq_population = _gm_1_4(0, rho, theta, phi)
-		print(qq_population, qq_hat)
-		print(rho, rho_hat)
-
-	if False:
-
-		tmp = [[], [], [], []]
-		for i in range(50):
-			print(i)
-			sample_gen = mrr(n=n_steps, m=m, rho=rho, theta=theta, phi=phi, sigma=sigma)
-			sample_list = list(zip(*sample_gen))
-			sample = sample_list[1]
-			vv_sample, kk_sample, qq_sample = _estimate_vkq(sample)
-			tmp[0].append(vv_sample)
-			tmp[1].append(kk_sample)
-			tmp[2].append(qq_sample)
-			a, b = _estimate_rho(sample, 3)
-			tmp[3].append(-1.*a*b)
-
-		vv_population = _gm_1_1(0, rho, theta, phi, sigma)
-		kk_population = _gm_1_3(0, rho, theta, phi, sigma)
-		qq_population = _gm_1_4(0, rho, theta, phi)
-		moments_population = [vv_population, kk_population, qq_population, qq_population]
-		for i in [0, 1, 2, 3]:
-			print(moments_population[i], np.mean(tmp[i]), np.std(tmp[i]), np.std(tmp[i])/np.mean(tmp[i]))
-
-	if False:
-		import matplotlib.pyplot as plt
-		plt.clf()
-		fig = plt.figure()
-		ax = fig.add_subplot(111)
-		ax.scatter([_gm_1_4(0, rho, theta, phi)]*len(tmp), tmp)
-		ax.set_xlim([-0.15,0.02])
-		ax.set_ylim([-0.15,0.02])
-		ax.set_xlabel(r'$Q(2)$')
-		ax.set_ylabel(r'$\hat{Q}(2)$')
-	#	plt.savefig('../plot/plot_04_q_2.pdf')
-		plt.show()
